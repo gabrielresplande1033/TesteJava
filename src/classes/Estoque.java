f7424f18a62bee4082eba9eab1ce1e174ebc028e
Gabriel <gbl_marcelino@hotmail.com>
2019-03-20 03:25:44 -0400

    ajustes nas funcionalidades e criacao do menu na main

--- src/classes/Estoque.java
+++ src/classes/Estoque.java
@@ -1,55 +1,54 @@
 package classes;
 
+import java.util.ArrayList;
+
 public class Estoque {
-	
+
 	private int tamanho;
 	private int quantidadeSessao;
 	private int tamanhoSessao;
-	Sessao []sessao;
-	
-	public Estoque(int quantidadeSessao, int tamanhoSessao){
+	Sessao[] sessao;
+
+	public Estoque(int quantidadeSessao, int tamanhoSessao) {
 		this.sessao = new Sessao[quantidadeSessao];
 		this.quantidadeSessao = quantidadeSessao;
 		this.tamanhoSessao = tamanhoSessao;
-		for(int i = 0; i < this.sessao.length; i++) {
+		for (int i = 0; i < this.sessao.length; i++) {
 			this.sessao[i] = new Sessao(tamanhoSessao);
 		}
 	}
-	
-	public void validarInsercaoProduto(String nomeProduto, int numeroSessao, int quantidadeProduto){
-		
+
+	public void inserirProduto(String nomeProduto, int numeroSessao, int quantidadeProduto) {
+
 		boolean validaPossibilidadeInsersao = false;
-		
+
 		boolean validaSeProdutoJaEstaInserido = false;
-		
+
 		int auxQuantidade = quantidadeProduto;
-		
+
 		int auxNumSessao = numeroSessao;
-		
-		forexterno : for(int i = numeroSessao; i < quantidadeSessao; i++) {
-			
-			if(sessao[i].getProduto(0) != null && sessao[i].getProduto(0).getNome() != nomeProduto) {
-				if(validaSeProdutoJaEstaInserido) {
+
+		forexterno: for (int i = numeroSessao; i < quantidadeSessao; i++) {
+
+			if (sessao[i].getProduto(0) != null && sessao[i].getProduto(0).getNome() != nomeProduto) {
+				if (validaSeProdutoJaEstaInserido) {
 					auxQuantidade = -1;
 					break;
 				}
-				auxNumSessao = i + 1; //se a posição nao for vazio e nem o produto a ser inserido, pule a sessao
+				auxNumSessao = i + 1; // se a posição nao for vazio e nem o produto a ser inserido, pule a sessao
 				continue;
-			}
-			else {
-				for(int j = 0; j < tamanhoSessao; j++) {
-					if(sessao[i].getProduto(j) == null) {
-                         --auxQuantidade;
-                         if(auxQuantidade == 0) {
-                        	 break forexterno;
-                         }
-                         
-						
-					}else if(sessao[i].getProduto(j).getNome() == nomeProduto) {
+			} else {
+				for (int j = 0; j < tamanhoSessao; j++) {
+					if (sessao[i].getProduto(j) == null) {
+						--auxQuantidade;
+						if (auxQuantidade == 0) {
+							break forexterno;
+						}
+
+					} else if (sessao[i].getProduto(j).getNome().equals(nomeProduto)) {
 						validaSeProdutoJaEstaInserido = true;
 						continue;
-					}
-					else {
+					} else {
 						auxNumSessao = i + 1;
 						auxQuantidade = quantidadeProduto;
 						break;
@@ -57,161 +56,281 @@ public class Estoque {
 				}
 			}
 		}
-		
-		if(auxQuantidade == 0) {
-		
-		forexterno1 : for(int i = auxNumSessao; i < quantidadeSessao ; i++) {
-			for(int j = 0; j < tamanhoSessao; j++) {
-				if(sessao[i].getProduto(j) != null) {
-					continue;
-				}
-				else {
-					sessao[i].produto[j] = new Produto(nomeProduto);
-					--quantidadeProduto;
-				}
-				
-				if(quantidadeProduto == 0) {
-					break forexterno1;
+
+		if (auxQuantidade == 0) {
+
+			forexterno1: for (int i = auxNumSessao; i < quantidadeSessao; i++) {
+				for (int j = 0; j < tamanhoSessao; j++) {
+					if (sessao[i].getProduto(j) != null) {
+						continue;
+					} else {
+						sessao[i].produto[j] = new Produto(nomeProduto);
+						--quantidadeProduto;
+					}
+
+					if (quantidadeProduto == 0) {
+						break forexterno1;
+					}
 				}
 			}
-		}
 		
-		}
-		else {
-			System.out.println("nao foi possivel inserir produto");
-		}
+		System.out.println("Produto inserido com sucesso!");
 		
+		try {
+			new Thread().sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		} else {
+			System.out.println("Não há espaço para o Produto Informado");
+			try {
+				new Thread().sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
-	
-	
+
 	public void listarEstoque() {
-		
-		for(int i = 0; i < quantidadeSessao; i++) {
-			for(int j = 0; j < tamanhoSessao; j++) {
-				if(sessao[i].getProduto(j) != null) {
+
+		for (int i = 0; i < quantidadeSessao; i++) {
+			for (int j = 0; j < tamanhoSessao; j++) {
+				if (sessao[i].getProduto(j) != null) {
 					System.out.printf(sessao[i].getProduto(j).getNome());
-				}
-				else {
+				} else {
 					System.out.printf("-");
 				}
 			}
 			System.out.printf("|");
 		}
 	}
-	
+
 	public boolean deletarProduto(String nome, int quantidadeASerDeletada) {
-		
+
 		int quantidadeEmEstoque = retornarQuantidadeProdutoEstoque(nome);
-		
-		System.out.printf("quantidadeestoque" + quantidadeEmEstoque);
-		
-		System.out.println();
-	    
-		if(quantidadeEmEstoque > 0 && quantidadeEmEstoque >= quantidadeASerDeletada) {
-			
-	  for1:	for(int i = quantidadeSessao - 1; i >= 0; i--) {
-				for(int j = tamanhoSessao -1; j >= 0; j--) {
-					if(sessao[i].getProduto(j) != null) {
-						if(sessao[i].getProduto(j).getNome() == nome) {
+
+		if (quantidadeEmEstoque > 0 && quantidadeEmEstoque >= quantidadeASerDeletada) {
+
+			for1: for (int i = quantidadeSessao - 1; i >= 0; i--) {
+				for (int j = tamanhoSessao - 1; j >= 0; j--) {
+					if (sessao[i].getProduto(j) != null) {
+						if (sessao[i].getProduto(j).getNome().equals(nome)) {
 							sessao[i].apagarProduto(j);
 							--quantidadeASerDeletada;
 						}
 					}
-					
-					if(quantidadeASerDeletada == 0) {
+
+					if (quantidadeASerDeletada == 0) {
 						break for1;
 					}
 				}
 			}
-		
-		     return true;
-		}else if(quantidadeEmEstoque < quantidadeASerDeletada){
+
+			return true;
+		} else if (quantidadeEmEstoque < quantidadeASerDeletada) {
 			System.out.println("Voce esta tentando deletar mais produtos do que há em estoque");
 			return false;
-		}else {
+		} else {
 			System.out.println("O produto informado não está no estoque");
 			return false;
 		}
 
 	}
-	
+
 	public int retornarQuantidadeProdutoEstoque(String nome) {
-	
+ 
 		int contadorDeProdutos = 0;
-		
-		forexterno: for(int i = 0; i < quantidadeSessao; i++) {
-			for(int j = 0; j < tamanhoSessao; j++) {
-               if(sessao[i].getProduto(j) != null) {
-            	   if(sessao[i].getProduto(j).getNome() == nome) {
-            		   contadorDeProdutos++;
-            	   }else if(contadorDeProdutos > 0) {
-            		   break forexterno;
-            	   }
-            	   
-               }else if(contadorDeProdutos > 0) {
-            	   break forexterno;
-               }
+
+		forexterno: for (int i = 0; i < quantidadeSessao; i++) {
+			for (int j = 0; j < tamanhoSessao; j++) {
+				if (sessao[i].getProduto(j) != null) {
+					if (sessao[i].getProduto(j).getNome().equals(nome)) {
+						contadorDeProdutos++;
+					} else if (contadorDeProdutos > 0) {
+						break forexterno;
+					}
+
+				} else if (contadorDeProdutos > 0) {
+					break forexterno;
+				}
 			}
 		}
-		
+
 		return contadorDeProdutos;
 	}
-	
+
 	public void mostrarStatusEstoque() {
-		
+
 		int auxQuantidadeEspacoLivreEstoque = 0;
-		
+
 		int quantidadeConsecutiva = 0;
-		
+
 		int maiorEspacoConsecutivoLivre = 0;
-		
+
 		boolean validarQuantidadeConsecutiva = true;
-		
-		for(int i = 0; i < quantidadeSessao; i++) {
-			for(int j = 0; j < tamanhoSessao; j++) {
-				
-				if(sessao[i].getProduto(j) == null && j == 0) {
+
+		for (int i = 0; i < quantidadeSessao; i++) {
+			for (int j = 0; j < tamanhoSessao; j++) {
+
+				if (sessao[i].getProduto(j) == null && j == 0) {
 					validarQuantidadeConsecutiva = true;
 				}
 				
-				if(sessao[i].getProduto(j) == null) {
+
+				if (sessao[i].getProduto(j) == null) {
 					auxQuantidadeEspacoLivreEstoque++;
-					if(validarQuantidadeConsecutiva) {
+					if (validarQuantidadeConsecutiva) {
 						quantidadeConsecutiva++;
-				    }
-		        }else {
-		        	if(quantidadeConsecutiva > maiorEspacoConsecutivoLivre) {
-		        		maiorEspacoConsecutivoLivre = quantidadeConsecutiva;
-		        	}
-		        	
-		        	validarQuantidadeConsecutiva = false;
-		        	quantidadeConsecutiva = 0;
-		        }
-				
-				
+					}
+
+					if(i == quantidadeSessao -1 && j == tamanhoSessao -1) {
+						if(quantidadeConsecutiva > maiorEspacoConsecutivoLivre) {
+							maiorEspacoConsecutivoLivre = quantidadeConsecutiva;
+						}
+					}
+				} else {
+					if (quantidadeConsecutiva > maiorEspacoConsecutivoLivre) {
+						maiorEspacoConsecutivoLivre = quantidadeConsecutiva;
+					}
+
+					validarQuantidadeConsecutiva = false;
+					quantidadeConsecutiva = 0;
+				}
+
 			}
-			
+
 		}
-		
+
 		System.out.printf("quantidadeEspacoLivre" + auxQuantidadeEspacoLivreEstoque);
 		System.out.println();
 		System.out.printf("quantidadeConsecutivo " + maiorEspacoConsecutivoLivre);
 		System.out.println();
 	}
-	
+
+	public void mostrarSessoesDisponiveis(int quantidadeASerArmazenado) {
+
+		boolean temSessaoDisponivel = false;
+
+		boolean sessaoDisponivel = false;
+
+		int aux = 0;
+
+		int quantidadeDeSessoesNecessarias = (quantidadeASerArmazenado / tamanhoSessao) + 1;
+
+		int sessoesDisponiveis[] = new int[quantidadeDeSessoesNecessarias];
+
+		int i = 0;
+
+		int auxI = i;
+
+		for (i = 0; i < quantidadeSessao; i++) {
+			if (sessao[i].getProduto(0) == null) {
+				++aux;
+			} else if (quantidadeDeSessoesNecessarias != aux) {
+				aux = 0;
+				auxI = i + 1;
+			}
+			auxI = i;
+
+			if (quantidadeDeSessoesNecessarias == aux) {
+				for (int j = 0; j < quantidadeDeSessoesNecessarias; j++) {
+					sessoesDisponiveis[j] = auxI;
+					auxI--;
+					sessaoDisponivel = true;
+				}
+
+				if (sessaoDisponivel) {
+					temSessaoDisponivel = true;
+					for (int k = quantidadeDeSessoesNecessarias - 1; k >= 0; k--) {
+						System.out.printf("Sessão " + sessoesDisponiveis[k] + " Disponível\n");
+					}
+					System.out.println("--------------------");
+					aux = 0;
+					sessaoDisponivel = false;
+					auxI = i + 1;
+
+				}
+			}
+		}
+
+		if (!temSessaoDisponivel) {
+			System.out.println("Não há sessões disponíveis para este tipo de produto");
+		}
+	}
+
+	public void realocarEstoque(Estoque estoque) {
+
+		String auxNome = "";
+
+		int auxContador = 0;
+
+		int auxIndexArrayProdutos = 0;
+
+		ArrayList<String> produtos = new ArrayList();
+
+		ArrayList<Integer> quantidadeProduto = new ArrayList();
+
+		int contadorDeProdutosDistintos = 0;
+
+		for (int i = 0; i < quantidadeSessao; i++) {
+			if (sessao[i].getProduto(0) != null && sessao[i].getProduto(0).getNome() != auxNome) {
+				produtos.add(sessao[i].getProduto(0).getNome());
+				contadorDeProdutosDistintos++;
+				auxNome = sessao[i].getProduto(0).getNome();
+			}
+		}
+
+		for1: for (int i = 0; i < quantidadeSessao; i++) {
+			for (int j = 0; j < tamanhoSessao; j++) {
+				if (sessao[i].getProduto(j) != null) {
+					if (sessao[i].getProduto(j).getNome() == produtos.get(auxIndexArrayProdutos)) {
+						auxContador++;
+					} else {
+						quantidadeProduto.add(auxContador);
+						auxContador = 1;
+						auxIndexArrayProdutos++;
+					}
+
+					if (i == quantidadeSessao - 1 && j == tamanhoSessao - 1 && auxContador > 0) {
+						quantidadeProduto.add(auxContador);
+						break for1;
+					}
+				} else if (i == quantidadeSessao - 1 && auxContador > 0) {
+					quantidadeProduto.add(auxContador);
+					break for1;
+				}
+
+			}
+		}
+		
+		estoque = new Estoque(quantidadeSessao, tamanhoSessao);
+
+		for (int i = 0; i < quantidadeProduto.size(); i++) {
+			estoque.inserirProduto(produtos.get(i), 0, quantidadeProduto.get(i));
+		}
+
+		System.out.println("ESTOQUE REALOCADO!!");
+
+		estoque.listarEstoque();
+
+	}
+
 	public int getTamanho() {
 		return tamanho;
 	}
+
 	public void setTamanho(int tamanho) {
 		this.tamanho = tamanho;
 	}
+
 	public Sessao getSessao(int numeroSessao) {
 		return sessao[numeroSessao];
 	}
+
 	public void setSessao(Sessao[] sessao) {
 		this.sessao = sessao;
 	}
-	
-	
-	
+
 }
--- src/classes/Teste.java
+++ src/classes/Teste.java
@@ -4,73 +4,201 @@ import java.util.Scanner;
 
 public class Teste {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+
 		boolean validaEstoque = false;
-		
+
 		Estoque estoque;
-		
+
 		int tamanhoEstoque, tamanhoSessao;
-	
+
 		do {
-			
+
 			Scanner scanner = new Scanner(System.in);
-			
+
 			System.out.println("Digite o tamanho do estoque");
-			
+
 			tamanhoEstoque = scanner.nextInt();
-			
+
 			System.out.println("Digite o tamanho da sessão");
-			
+
 			tamanhoSessao = scanner.nextInt();
-			
-			if(tamanhoEstoque % tamanhoSessao == 0) {
+
+			if (tamanhoEstoque % tamanhoSessao == 0) {
 				validaEstoque = true;
-				scanner.close();
 			}
-			
+
 			else {
 				System.out.println("Não foi possível criar as sessões com tal quantidade, informe novos valores:");
 			}
-			
-			
-		}while(!validaEstoque);
-		
+
+		} while (!validaEstoque);
+
+		System.out.println("Estoque criado com sucesso");
+
 		int quantidadeDeSessao = tamanhoEstoque / tamanhoSessao;
-		
+
 		estoque = new Estoque(quantidadeDeSessao, tamanhoSessao);
-		
-		
-		estoque.validarInsercaoProduto("q", 0, 7);
-		
-		estoque.validarInsercaoProduto("q", 0, 2);
-		
-		estoque.validarInsercaoProduto("q", 0, 2);
-		
-        estoque.validarInsercaoProduto("j", 0, 4);
-        
-    	estoque.validarInsercaoProduto("q", 0, 1);
-		
-		estoque.validarInsercaoProduto("l", 0, 1);
-		
-		estoque.validarInsercaoProduto("m", 0, 3);
-		
-		estoque.validarInsercaoProduto("k", 0, 1);
-		
-		estoque.validarInsercaoProduto("m", 0, 2);
-		
-		estoque.validarInsercaoProduto("o", 0, 3);
-		
-		estoque.deletarProduto("l", 1);
-		
-		estoque.deletarProduto("k", 1);
-		
-		estoque.listarEstoque();
-		
-		estoque.mostrarStatusEstoque();
-		
-		System.out.println();
-	
+
+		new Thread().sleep(1000);
+
+		int operacao = 1;
+
+		do {
+
+			boolean validacao = false;
+
+			System.out.println();
+
+			System.out.println("--------------------------------------------");
+
+			System.out.println("MENU DA APLICAÇÃO");
+
+			System.out.println("--------------------------------------------");
+
+			System.out.println("1 - Inserir Produto");
+
+			System.out.println("2 - Deletar Produto");
+
+			System.out.println("3 - Listar Estoque");
+
+			System.out.println("4 - Mostrar Status do Estoque");
+
+			System.out.println("5 - Mostrar Sessões Disponíveis para um novo produto");
+
+			System.out.println("6 - Realocar Estoque");
+
+			System.out.println("7 - Sair da Aplicação");
+
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("Digite o número da operação que deseja realizar para prosseguir:");
+
+			do {
+				if (scanner.hasNextInt()) {
+					operacao = scanner.nextInt();
+					validacao = true;
+				} else {
+					scanner.nextLine();
+					System.out.println("Deve ser inserido um numero de 1 a 7, digite novamente:");
+				}
+			} while (!validacao);
+
+			scanner.nextLine();
+
+			switch (operacao) {
+			case 1:
+				String nomeProduto = "";
+				int numeroSessao = 0, quantidadeProduto = 0;
+
+				System.out.println("Digite o nome do Produto que deseja inserir:");
+				nomeProduto = scanner.nextLine();
+
+				while (nomeProduto.isEmpty()) {
+					scanner.next();
+					System.out.println("Parametro digitado incorretamente, digite novamente:");
+					nomeProduto = scanner.nextLine();
+				}
+
+				System.out.println("Digite o numero da Sessao que deseja inserir o Produto");
+				validacao = false;
+
+				do {
+					if (scanner.hasNextInt()) {
+						numeroSessao = scanner.nextInt();
+						validacao = true;
+					} else {
+						scanner.nextLine();
+						System.out.println("Parametro digitado incorretamente, digite novamente:");
+					}
+				} while (!validacao);
+				System.out.println("Digite a quantidade do produto a ser inserido:");
+				validacao = false;
+
+				do {
+					if (scanner.hasNextInt()) {
+						quantidadeProduto = scanner.nextInt();
+						validacao = true;
+					} else {
+						scanner.nextLine();
+						System.out.println("Parametro digitado incorretamente, digite novamente:");
+					}
+				} while (!validacao);
+				estoque.inserirProduto(nomeProduto, numeroSessao, quantidadeProduto);
+				scanner.nextLine();
+				break;
+
+			case 2:
+				String nomeProdutoASerDeletado = "";
+				int quantidadeProdutoASerDeletado = 0;
+
+				System.out.println("Digite o nome do Produto que deseja Excluir:");
+				nomeProdutoASerDeletado = scanner.nextLine();
+
+				while (nomeProdutoASerDeletado.isEmpty()) {
+					scanner.next();
+					System.out.println("Parametro digitado incorretamente, digite novamente:");
+					nomeProdutoASerDeletado = scanner.nextLine();
+				}
+
+				validacao = false;
+
+				System.out.println("Digite a quantidade que produto a ser deletada:");
+
+				do {
+					if (scanner.hasNextInt()) {
+						quantidadeProdutoASerDeletado = scanner.nextInt();
+						validacao = true;
+					} else {
+						scanner.nextLine();
+						System.out.println("Parametro digitado incorretamente, digite novamente:");
+					}
+				} while (!validacao);
+				if (estoque.deletarProduto(nomeProdutoASerDeletado, quantidadeProdutoASerDeletado)) {
+					System.out.println("PRODUTO DELETADO COM SUCESSO");
+					new Thread().sleep(1000);
+				} else {
+					System.out.println("NAO FOI POSSIVEL DELETAR O PRODUTO");
+					new Thread().sleep(2000);
+				}
+
+				break;
+
+			case 3:
+				estoque.listarEstoque();
+				new Thread().sleep(1000);
+				break;
+			case 4:
+				estoque.mostrarStatusEstoque();
+				new Thread().sleep(1000);
+				break;
+			case 5:
+				validacao = false;
+
+				int quantidadeASerArmazenado = 0;
+				System.out.println("Digite a quantidade a ser armazenada:");
+				do {
+					if (scanner.hasNextInt()) {
+						quantidadeASerArmazenado = scanner.nextInt();
+						validacao = true;
+					} else {
+						scanner.nextLine();
+						System.out.println("Parametro digitado incorretamente, digite novamente:");
+					}
+				} while (!validacao);
+				estoque.mostrarSessoesDisponiveis(quantidadeASerArmazenado);
+				new Thread().sleep(2000);
+				break;
+			case 6:
+				estoque.realocarEstoque(estoque);
+				new Thread().sleep(3000);
+				break;
+			case 7:
+				System.out.println("SAINDO DA APLICAÇÃO...");
+				new Thread().sleep(2000);
+			}
+
+		} while (operacao != 7);
 
 	}
 
